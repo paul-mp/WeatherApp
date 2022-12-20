a7556a2ce62edfa542c03b87d670dd4971ccf72a
Paul <pavel.maslov01@gmail.com>
2022-12-20 11:04:22 -0500

    initial commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ data = response.json()
 user_Location = f"{data['city']}"
 long = f"{data['lon']}"
 lat = f"{data['lat']}"
+API_KEY = open('api_key', "r").read()
 
 
 # Converts kelvin to both Celsius and Fahrenheit
@@ -21,8 +22,8 @@ def kelvin_to_c_and_f(kelvin):
 
 
 # Takes the users current location
-def location_gather(long, lat, city):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid=e5bd1a889b363d608b95e43875f020c2"
+def location_gather(long, lat, city, API_KEY):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}"
     response = requests.get(url)
     data = json.loads(response.text)
 
@@ -32,7 +33,4 @@ def location_gather(long, lat, city):
     return f"Temperature in {city} is: {temp_celsius.__round__()}℃ or {temp_fahrenheit.__round__()}℉"
 
 
-print(location_gather(long, lat, user_Location))
-
-
-
+print(location_gather(long, lat, user_Location, API_KEY))
